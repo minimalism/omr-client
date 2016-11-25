4e518cdda62c90c58c0d753c8ce1f9c4cb2cebb6
minimalism <andreascarlson@gmail.com>
2016-11-25 14:45:24 +0100

    added databinding to games list

--- src/app/components/games.tsx
+++ src/app/components/games.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
 
-export default class GamesList extends React.Component<{}, {}> {
+class GamesListState {
+    gameIds : string[] = [];
+}
+
+export default class GamesList extends React.Component<{}, GamesListState> {
+
+    ref : firebase.database.Reference;
+
+    constructor(props){
+        super(props);
+        this.state = new GamesListState();
+        this.ref = firebase.database().ref().child('/games');
+    }
 
     render(){
-        return (<Game />);
+        return (
+            <div>
+            { this.state.gameIds.map((gameId) => { return <Game id={ gameId }/> }) }
+            </div>
+        );
+    }
+
+    gameAdded(gameData){
+        var state = this.state;
+        state.gameIds.push(gameData.key);
+        this.setState(state);
+    }
+
+    gameRemoved(gameData){
+        alert("todo");
+    }
+
+    componentWillMount(){
+        this.ref.on('child_added', this.gameAdded.bind(this));
+        this.ref.on('child_removed', this.gameRemoved.bind(this));
+    }
+
+    componentWillUnmount(){
+        this.ref.off();
     }
 }
 
-class Game extends React.Component<{}, {}> {
+interface GameProperties {
+    id : string;
+}
+
+class Game extends React.Component<GameProperties, {}> {
+
+    constructor(props){
+        super(props);
+    }
+
     render(){
-        return (<div>Here's a game innit</div>);
+        return (<div>Here's game { this.props.id }</div>);
     }
 }
